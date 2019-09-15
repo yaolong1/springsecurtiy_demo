59edc749fbddb35c0115129d7dce374a2ae068bf
“wyaolong <1638538651@qq.com>
2019-09-15 22:50:53 +0800

    认证服务器

---
+++ spring-security-oauth2/spring_security_oauth2/spring-security-oauth2-server/src/main/java/com/yaolong/oauth2/Auth2ServerApplication.java
@@ -0,0 +1,16 @@
+package com.yaolong.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 殷跃龙
+ * @Description:
+ * @Date: Create in 22:32 2019/9/15
+ */
+@SpringBootApplication
+public class Auth2ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Auth2ServerApplication.class,args);
+    }
+}
---
+++ spring-security-oauth2/spring_security_oauth2/spring-security-oauth2-server/src/main/java/com/yaolong/oauth2/server/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,11 @@
+package com.yaolong.oauth2.server.config;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ * @Author: 殷跃龙
+ * @Description:
+ * @Date: Create in 22:36 2019/9/15
+ */
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+}
